833daba2d66a02ff48fae7512d108578bbf273c5
CristhianCezar <cristhiancezardev@gmail.com>
2023-02-25 15:50:49 -0300

    Updates

---
+++ precache-manifest.266a644230528a7d062188f8f9039bdd.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "aed0f54bfed2eb23b5ea",
+    "url": "/lift-detox-caps/static/css/main.f0f6cc1f.chunk.css"
+  },
+  {
+    "revision": "aed0f54bfed2eb23b5ea",
+    "url": "/lift-detox-caps/static/js/main.aed0f54b.chunk.js"
+  },
+  {
+    "revision": "91b4e6e336f9c52afa92",
+    "url": "/lift-detox-caps/static/js/1.91b4e6e3.chunk.js"
+  },
+  {
+    "revision": "7059080172977db019cf",
+    "url": "/lift-detox-caps/static/js/runtime~main.70590801.js"
+  },
+  {
+    "revision": "34fbfe859527bfc6b7ee83f42fd74441",
+    "url": "/lift-detox-caps/static/media/iconPlant.34fbfe85.svg"
+  },
+  {
+    "revision": "7061f03da913f8e67de35b2d66ce45b2",
+    "url": "/lift-detox-caps/static/media/whatsappIcons.7061f03d.png"
+  },
+  {
+    "revision": "9d6561aad0472a109fa0f61d10804ed3",
+    "url": "/lift-detox-caps/static/media/segredoDaFormula.9d6561aa.png"
+  },
+  {
+    "revision": "7b47f329f33767fbcf96d9e8ede2e5ae",
+    "url": "/lift-detox-caps/static/media/diminuirApetite.7b47f329.jpg"
+  },
+  {
+    "revision": "dd65abe652416d442f8b75efd9b86a56",
+    "url": "/lift-detox-caps/static/media/intestinoRegulado.dd65abe6.jpg"
+  },
+  {
+    "revision": "bb934f2da969fbaea334925a05f865c9",
+    "url": "/lift-detox-caps/static/media/mulherFazendoAlongamentoNaRua.bb934f2d.jpeg"
+  },
+  {
+    "revision": "f827bed8678c7af177ecb79fe258f396",
+    "url": "/lift-detox-caps/static/media/liftDetoxGarantia.f827bed8.png"
+  },
+  {
+    "revision": "9ba305d3768bff7bbc1ae01ec0041b1b",
+    "url": "/lift-detox-caps/static/media/LiftDetoxCaps.9ba305d3.jpg"
+  },
+  {
+    "revision": "5a9d78d8cf5ded168fe642575c3a4087",
+    "url": "/lift-detox-caps/index.html"
+  }
+];
